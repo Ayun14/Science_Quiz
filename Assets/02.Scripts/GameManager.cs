15386e56cdbf1ccbd6218072486c48aab950cf0b
AYUNKIM <ayoonkim07@naver.com>
2023-08-31 00:26:54 +0900

    시작 및 소개 UI 준비

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int quizCount = 0;
+
+    [SerializeField] private TextMeshProUGUI quizText = null;
+
+    [SerializeField] private GameObject startPanel = null;
+    [SerializeField] private GameObject introducePanel = null;
+
+    [SerializeField] private GameObject oImage;
+    [SerializeField] private GameObject xImage;
+
+    private void Start()
     {
-        
+        oImage.SetActive(false);
+        xImage.SetActive(false);
+
+        introducePanel.SetActive(false);
+        startPanel.SetActive(true);
+
+        quizCount = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartButtonClick()
     {
-        
+        oImage.SetActive(true);
+        xImage.SetActive(true);
+
+        introducePanel.SetActive(false);
+    }
+
+    public void IntroduceButtonClick()
+    {
+        introducePanel.SetActive(true);
+        startPanel.SetActive(false);
+    }
+
+    public void ExitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator StageChange()
+    {
+        yield return null;
     }
 }
